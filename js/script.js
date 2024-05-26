608cff205ee9fe04b974bf3dc1e90e5d43cc9d3f
lucaspinheiror <pinheiroi338@gmail.com>
2024-05-26 01:41:02 -0300

    inserindo overlay e tirando rolagem

--- js/script.js
+++ js/script.js
@@ -1,32 +1,46 @@
 const minus = document.querySelector('#minus');
 const plus = document.querySelector('#plus');
 const qtd = document.querySelector('#quantia span');
+
+let qtdProdutos = 0;
+
 const burguer = document.querySelector('#menu-burguer'); 
-const btnCloseMenu = document.querySelector('#btn-menu')
+const btnCloseMenu = document.querySelector('#btn-menu');
 const menu = document.querySelector('#menu');
 
+const body = document.body;
+
+// FUNÇÕES DE ABRIR E FECHAR MENU
 
 burguer.addEventListener('click', abrirMenu);
 btnCloseMenu.addEventListener('click', fecharMenu);
 
-// FUNÇÃO ABRIR MENU
-
 function abrirMenu(){
     menu.classList.add('abrir');
+
+    document.querySelector('#overlay').classList.add('active'); // FUNDO ESCURO 
+
+    if(menu.style.overflow = 'auto'){
+        body.style.overflow = 'hidden'   // IMPEDIR ROLAGEM COM MENU ABERTO
+    } 
 }
 
 function fecharMenu(){
     menu.classList.remove('abrir');
+
+    document.querySelector('#overlay').classList.remove('active');
+
+    if(menu.style.overflow = 'hidden'){
+        body.style.overflow = 'auto';
+    }   
 }
 
-// CRIANDO FUNÇÕES DE ADICIONAR E REMOVER PRODUTO
+// FUNÇÕES DO SLIDER
 
-let qtdProdutos = 0;
+// FUNÇÕES DE ADICIONAR E REMOVER PRODUTO
 
 plus.addEventListener('click', adicionarProduto);
-minus.addEventListener('click', removerProduto);
-
-// FUNÇÃO DE ADICIONAR PRODUTOS
+minus.addEventListener('click', reduzirProduto);
 
 function adicionarProduto(){
 
@@ -34,18 +48,14 @@ function adicionarProduto(){
         qtdProdutos++;
     }
    
-   qtd.textContent = qtdProdutos; // ATUALIZA O ELEMENTO DOM
- 
+   qtd.textContent = qtdProdutos; // ATUALIZA O ELEMENTO DOM 
 }
 
-// FUNÇÃO DE REMOVER PRODUTOS
 
-function removerProduto(){
+function reduzirProduto(){
     if(qtdProdutos > 0){
         qtdProdutos--;
     }
 
     qtd.textContent = qtdProdutos; // ATUALIZA O ELEMENTO DOM
 }
-
- 
